8009ffc2f4262375fefded92f00193db22afdcd0
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-23 13:19:36 +0900

    アプリのメインコンポーネントをimport

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,7 @@
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/style.css";
+import Header from "./Components/Header"
+import Footer from "./Components/Footer
+import { BrowserRouter as Router, ROute, Switch } from "react-route-dom"
