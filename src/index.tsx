19869c59261dfdb2a39f746068934b2b6638a76f
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-23 13:27:12 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"; // アプリのえみんコンポーネントを取り込む
 import App from "./App";
 import "./styles/style.css";
 import Header from "./Components/Header"
 import Footer from "./Components/Footer
 import { BrowserRouter as Router, ROute, Switch } from "react-route-dom"
+
+const rootElement = document.getElement.By.Id("root"); // index.html内の<div id="root">を取得する
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);　// reactDOM.createRootを使い、ルートを作成して、アプリケーションをレンダリングする
+  root.render(
+    <React.StrictMode> 
+      <App /> 
+    </React.StrictMode> //開発中に潜在的なエラーを検出する為に使用する
+    );
+}
