af5ce1c8e07f1bf9c8849540f40f42a87c883e63
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-23 15:19:16 +0900

    headerのコンポーネント作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom"; // react router使用する？
+import "./Header.css"; 
 
+const Header: React.FC = () => {
+    return (
+        <header className="header">
+            <div className="container">
+                <h1 className="logo">My Application</h1>
+                <nav>
+                    <ul className="nav-links">
+                        <li><Link to="/">ホーム</Link></li>
+                        <li><Link to="/about">アバウト</Link></li>
+                        <li><Link to="/contact">お問い合わせ</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
