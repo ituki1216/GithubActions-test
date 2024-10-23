11c71c4a493d3e9e67ab0a4febf7ae636c8ed62c
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-23 15:16:14 +0900

    footerのコンポーネント作成

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,1 +1,20 @@
+// Webアプリケーションのフッター部分に表示される情報やリンクなどを含む内容を記載
 
+import Reaact from "react";
+import "./Footer.css;
+
+const Footer: React.FC = () => {
+  rerutn (
+    <footer className="footer">
+      <div className="container">
+        <p>@ {name Date().getFullYear()} です。</p> 
+        <ul>
+            <li><a href="/privacy">プライバシーポリシー</a></li>
+            <li><a href="/terms">利用規約</a></li>
+        </ul>
+      </div>
+    </footer> 
+    );
+};
+
+export default Footer;
