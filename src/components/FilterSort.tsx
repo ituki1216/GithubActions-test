3087a9f6d0a36ffe18175561c1bb28358e61d7f6
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-24 23:01:50 +0900

    アイテム型のオブジェクトの配列を定義

--- src/components/FilterSort.tsx
+++ src/components/FilterSort.tsx
@@ -4,3 +4,12 @@ interface Item {
   id: number;
   name: string;
 }
+
+// アイテム型のオブジェクトの配列を定義 配列の中にはnameとidが格納されいる
+const items: Item[] = [
+  { id: 1, name: "勇者の剣" },
+  { id: 2, name: "魔法の盾" },
+  { id: 3, name: "木の枝" },
+  { id: 4, name: "鋼のつるぎ" }
+  ]; 
+  
