6b3ba29e70846ee13dda474cff8b3b429ac5787d
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-24 23:40:31 +0900

    Update ProductDetail.tsx

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,1 +1,20 @@
+import React from 'react';
 
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+}
+
+const ProductDetail: React.FC<{ product: Product }> = ({ product }) => {
+  return (
+    <div>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p>Price: ${product.price}</p>
+    </div>
+  );
+};
+
+export default ProductDetail;
