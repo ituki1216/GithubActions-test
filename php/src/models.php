70af9c5b4241eb760565ff7adb96e4a9e1a6e6b6
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-29 15:55:39 +0900

    データベースの定義

--- php/src/models.php
+++ php/src/models.php
@@ -1,1 +1,9 @@
+<?php
 
+class Database {
+    private $host = 'localhost';
+    private $db_name = 'ec_site';
+    private $username = 'root';
+    private $password = '';
+    private $connection;
+}
