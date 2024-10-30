8c4527d4ec748162c0a0b4b593a5b3a854f404ae
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-30 14:52:15 +0900

    add関数: 非同期処理

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -38,5 +38,34 @@ const addTodo = async () => { // 非同期処理addTodo
  useEffect(() => {
         fetchTodos();
     }, []);
+ 
+  return (
+        <div>
+            <h1>TODO List</h1>
+            <input
+                type="text"
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+                placeholder="New TODO"
+            />
+            <button onClick={addTodo}>Add</button>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>
+                        <span
+                            style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+                            onClick={() => updateTodo(todo)}
+                        >
+                            {todo.title}
+                        </span>
+                        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default TodoList;
 
  
