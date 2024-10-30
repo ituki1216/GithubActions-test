301fc540a0999b2930b827c577fd554f6d5b1599
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-30 11:13:30 +0900

    Update routes.php

--- php/src/routes.php
+++ php/src/routes.php
@@ -1,2 +1,6 @@
 use App\Http\Controllers\TodoController;
 
+Route::get("/todos", [TodoContoroller::class, "index"]);　# todoにアクセスされたときにtodoデータを取得する
+Route::post('/todos', [TodoController::class, 'store']);
+Route::put('/todos/{id}', [TodoController::class, 'update']);
+Route::delete('/todos/{id}', [TodoController::class, 'destroy']);　
